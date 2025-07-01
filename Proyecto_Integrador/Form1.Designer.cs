e83e3f2c7f9841a2cd61b0535313d8fc201bcb36
pillo <pillo@PILLO>
2025-07-01 13:49:18 -0600

    yopi

--- Proyecto_Integrador/Form1.Designer.cs
+++ Proyecto_Integrador/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -93,11 +94,21 @@
             this.label4.TabIndex = 5;
             this.label4.Text = "HOLAAAAA";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(369, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 90);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -119,6 +130,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
     }
 }
 
