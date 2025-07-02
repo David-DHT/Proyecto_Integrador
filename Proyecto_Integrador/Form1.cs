3c37872def90f5ccbd592c9253a3aa5088c531fe
dht04 <dht04@LENOVO_DHT>
2025-07-01 20:53:09 -0600

    David

--- Proyecto_Integrador/Form1.cs
+++ Proyecto_Integrador/Form1.cs
@@ -16,5 +16,10 @@ namespace Proyecto_Integrador
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
